ab12b5f7052e88a0709a8a90bdd80c5401aca6e4
Kameshwar <kameshwarkdm@gmail.com>
2026-03-01 15:16:32 +0530

    Test routing

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
-const app = require('../server/index.js');
+const express = require('express');
+const app = express();
+app.all('*', (req, res) => {
+    res.json({
+        message: 'Test route',
+        method: req.method,
+        url: req.url,
+        originalUrl: req.originalUrl,
+        path: req.path
+    });
+});
 module.exports = app;
